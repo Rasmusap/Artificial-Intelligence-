b9e9848ea3412e8a950c1af3b4580faa410f4103
Alihamza2600 <s235479@dtu.dk>
2026-03-16 19:23:12 +0100

    removed old src

--- old_src/src/agent/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from src.game.state import GameState
-
-
-class BaseAgent(ABC):
-    @abstractmethod
-    def select_move(self, state: GameState) -> int:
-        raise NotImplementedError
--- old_src/src/common/types.py
+++
@@ -1,3 +0,0 @@
-from typing import Literal
-
-Player = Literal["X", "O"]
